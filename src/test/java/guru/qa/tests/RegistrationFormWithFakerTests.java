b0d8e1daaabad526e0a0e4aaaceded28688b4103
Yuliya Kudrina <y.kudrina@y-kudrina-mac.local>
2022-08-23 14:17:12 +0300

    optimization

--- src/test/java/guru/qa/tests/RegistrationFormWithFakerTests.java
+++ src/test/java/guru/qa/tests/RegistrationFormWithFakerTests.java
@@ -1,43 +1,28 @@
 package guru.qa.tests;
 
-import com.codeborne.selenide.Configuration;
 import guru.qa.pages.RegistrationFormPage;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import com.github.javafaker.Faker;
-import java.util.Locale;
+
 
 import static java.lang.String.format;
-import static com.codeborne.selenide.Selenide.$;
 
 public class RegistrationFormWithFakerTests extends TestBase {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
     Faker faker = new Faker();
-    String firstName, lastName, email, gender, mobile, day, month, year, hobbie, file, address, state, city;
-
-    @BeforeEach
-    void prepareTestData() {
-        firstName = faker.name().firstName();
-        lastName = faker.name().lastName();
-        email = faker.internet().emailAddress();
-        gender = "Other";
-        mobile = faker.phoneNumber().subscriberNumber(10);
-        day = faker.number().numberBetween(1, 30) + "";
-        month = "July"; // не смогла определить месяц через Faker
-        year = faker.number().numberBetween(1950, 2000) + "";
-        hobbie = "Music";
-        file = "Floppa.png";
-        address = faker.address().fullAddress().substring(7);
-        state = "NCR";
-        city = "Delhi";
-    }
-
-    @BeforeAll
-    static void configure() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-    }
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    String email = faker.internet().emailAddress();
+    String gender = "Other";
+    String mobile = faker.phoneNumber().subscriberNumber(10);
+    String day = faker.number().numberBetween(1, 30) + "";
+    String month = "July"; // не смогла определить месяц через Faker
+    String year = faker.number().numberBetween(1950, 2000) + "";
+    String hobbie = "Music";
+    String file = "Floppa.png";
+    String address = faker.address().fullAddress().substring(7);
+    String state = "NCR";
+    String city = "Delhi";
 
 
     @Test
